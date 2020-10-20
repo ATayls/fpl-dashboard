985e4c398441cf0d643e4a8b621ad344e0de2114
ATayls <adam.taylor300@yahoo.co.uk>
2020-10-20 23:03:43 +0100

    Update layouts, Add functioning loading progress
    Updated layouts to house input buttons for league id and various components within bootstrap grid to display results
    
    Add a functioning loop between multiple callbacks that allows running of a slow function (request for manager data) in a loop and updates the relevant progress bar component as it progresses.

--- src/layouts.py
+++ src/layouts.py
@@ -1,48 +1,116 @@
+from dash import callback_context
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import dash_table
 
 from app import app
+from fpl_api_utils import league_dataframe
 
 
 control = dbc.Row(
-    dbc.Card(
-        dbc.CardBody(
-            [
-                dbc.Input(id="input", placeholder="Input League Id...", type="number"),
+    [
+        html.Div(id='manager-list', style={'display': 'none'}),
+        html.Div(id='manager-df-path', style={'display': 'none'}),
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    dbc.Input(id="league-id", placeholder="Input League Id...", type="number"),
+                    dbc.Button(
+                        "Load League Data",
+                        color="primary",
+                        block=True,
+                        id="run-button",
+                    ),
+                ]
+            ),
+            outline=False,
+            color='light',
+            className="w-100 mb-3",
+        ),
+    ]
+)
+
+analysis = html.Div(
+    [
+        dbc.Row(
+            dbc.Col(
+                html.H1(id='league-name', className='text-center font-weight-bold'),
+                width={'size': 6, 'offset': 3}
+            ),
+        ),
+        dbc.Row(
+            dbc.Col(
                 dbc.Button(
-                    "Generate for League Id",
+                    "Display League Table",
+                    id="collapse-button",
+                    className="mb-3 text-center",
                     color="primary",
                     block=True,
-                    id="button1",
                 ),
-            ]
+                width={'size': 4, 'offset': 4},
+            ),
         ),
-        outline=False,
-        color='light',
-        className="w-100 mb-3",
-    ),
+        dbc.Row(
+            dbc.Col(
+                dbc.Collapse(
+                    dbc.Card(
+                        dash_table.DataTable(id='league-table')
+                    ),
+                    id="collapse",
+                ),
+            ),
+            justify='center'
+        ),
+        dbc.Row(
+            [
+                dbc.Tabs(
+                    [
+                        dbc.Tab(label="League Rankings", tab_id="rank"),
+                        dbc.Tab(label="Percentage Ownership", tab_id="prc_own"),
+                        dbc.Tab(label="Transfers In", tab_id="trans_in"),
+                        dbc.Tab(label="Transfers Out", tab_id="trans_out"),
+                        dbc.Tab(label="Captains", tab_id="captains"),
+                        dbc.Tab(label="Manager Correlation", tab_id="man_corr"),
+                        dbc.Tab(label="Manager Points", tab_id="man_box"),
+
+                    ],
+                    id="tabs",
+                    active_tab="prc_own",
+                    className='mt-3'
+                ),
+                html.Div(id="tab-content"),
+            ]
+        )
+    ],
+    id='analysis-div',
+    style={'display': 'none'}
 )
 
 
-analysis = html.Div([
-    dbc.Row(
-        [
-            dbc.Tabs(
-                [
-                    dbc.Tab(label="League Rankings", tab_id="rank"),
-                    dbc.Tab(label="Percentage Ownership", tab_id="prc_own"),
-                    dbc.Tab(label="Transfers In", tab_id="trans_in"),
-                    dbc.Tab(label="Transfers Out", tab_id="trans_out"),
-                    dbc.Tab(label="Captains", tab_id="captains"),
-                    dbc.Tab(label="Manager Correlation", tab_id="man_corr"),
-                    dbc.Tab(label="Manager Points", tab_id="man_box"),
+@app.callback(
+    Output("collapse", "is_open"),
+    [Input("collapse-button", "n_clicks")],
+    [State("collapse", "is_open")],
+)
+def toggle_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
 
-                ],
-                id="tabs",
-                active_tab="prc_own",
-            ),
-            html.Div(id="tab-content"),
-        ]
-    )
-])
+
+@app.callback(
+    [Output("league-table", "data"), Output("league-table", "columns"),
+     Output("manager-list", "children"), Output("analysis-div", "style"),
+     Output("league-name", "children")],
+    [Input("run-button", "n_clicks"), State("league-id", "value")]
+)
+def run(n_clicks, l_id):
+    ctx = callback_context
+    if not ctx.triggered:
+        raise PreventUpdate
+    league_df, name = league_dataframe(l_id)
+    columns = [{"name": i, "id": i} for i in league_df.columns]
+    manager_list = league_df['entry'].to_list()
+    return league_df.to_dict('records'), columns, manager_list, {'display': 'block'}, name
--- src/loading_loop.py
+++ src/loading_loop.py
@@ -1,8 +1,74 @@
 import dash_bootstrap_components as dbc
 import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 
-progress = dbc.Row(
+import pandas as pd
+
+from app import app, DATA_STORE
+from fpl_api_utils import scrape_manager_team
+
+progress = html.Div(
     [
-        html.H1("Progress"),
+        dcc.Interval(id="progress-interval", interval=0.5*1000, disabled=False),
+        html.Div(id="latest-output", style={'display': 'none'}),
+        html.Div(id="progress-output", style={'display': 'none'}),
+        html.Div(0, id="run-index", style={'display': 'none'}),
+        html.Div(id="load-complete", style={'display': 'none'}),
+        dbc.Progress(id="progress", striped=True, className='mb-3'),
     ]
 )
+
+
+@app.callback(
+    Output("run-index", "children"),
+    [Input("progress-interval", "n_intervals"), State("latest-output", "children"), State("run-index", "children")]
+)
+def check(interval_trigger, latest_output, run_index):
+    if run_index != latest_output:
+        raise PreventUpdate
+    else:
+        return run_index+1
+
+
+@app.callback(
+    [Output("progress", "value"), Output("latest-output", "children"),
+     Output("manager-df-path", "children"), Output("progress", "children")],
+    [Input("manager-list", "children"), Input("run-index", "children"),
+     State("session_id", "children"), State("manager-df-path", "children")]
+)
+def run(list_input, run_index, session_id, df_path):
+    if list_input is None:
+        raise PreventUpdate
+
+    entry_id = list_input[run_index]
+    gw_list = [1, 2, 3]
+
+    manager_df = scrape_manager_team(entry_id, gw_list)
+    manager_df['manager'] = entry_id
+
+    if df_path is not None:
+        stored_df = pd.read_feather(df_path)
+        manager_df = pd.concat([stored_df, manager_df]).reset_index(drop=True)
+    else:
+        df_path = DATA_STORE.joinpath(session_id, 'manager_df.feather')
+
+    manager_df.to_feather(df_path)
+    progress_val = ((run_index+1)/len(list_input))*100
+    progress_str = f"{run_index+1}/{len(list_input)}" if progress_val >= 5 else ""
+    return progress_val, run_index, str(df_path), progress_str
+
+
+@app.callback(
+    [Output("progress-interval", "disabled"), Output("load-complete", "children")],
+    [Input("manager-list", "children"), Input("progress", "value")]
+)
+def stop_interval(list_input, progress):
+    if list_input is None:
+        raise PreventUpdate
+
+    if progress >= 100:
+        return True, True
+    else:
+        return False, False
